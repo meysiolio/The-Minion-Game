7d21c19bc28c6adc47fee0949b21ec6c092d9e92
Meysiolio <saturninsky@gmail.com>
2021-09-27 13:25:05 +0330

    test commit

--- Minion-Game.py
+++ Minion-Game.py
@@ -2,35 +2,20 @@ def minion_game(string):
     # your code goes here
     kevin = 0
     stuart = 0
-    L = []
+
     for i in range(len(string)):
-        for j in range(i+1,len(string)+1):
-        
-            if len(string[i:j]) <= 0 or string[i:j] in L:
-                break
+        if string[i][0] in 'AEIOUaeiou':
+            kevin += len(string) - i 
+        else:
+            stuart += len(string) - i
 
-            print(f'{string[i:j]} {count_substring(string,string[i:j])}')
-
-            if string[i:j][0] in 'AEIOUaeiou':
-                kevin += count_substring(string,string[i:j])
-            else:
-                stuart += count_substring(string,string[i:j])
-
-            L.append(string[i:j])
         
     if kevin > stuart:
         print(f'Kevin {kevin}')
     elif kevin == stuart:
-        print(f'Draw in {stuart}')
+        print('Draw')
     else:
-        print(f'Stuart {stuart} {kevin}')
-
-def count_substring(string, sub_string):
-    count = 0
-    for i in range(len(string)):
-        if string[i:].startswith(sub_string):
-            count += 1
-    return count
+        print(f'Stuart {stuart}')
 
 if __name__ == '__main__':
     s = input()
