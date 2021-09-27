fc25914f8af4e4db6d14a2d6c21125e873074e75
Meysiolio <saturninsky@gmail.com>
2021-09-27 12:59:34 +0330

    add new count function

--- Minion-Game.py
+++ Minion-Game.py
@@ -5,22 +5,32 @@ def minion_game(string):
     L = []
     for i in range(len(string)):
         for j in range(i+1,len(string)+1):
-            #print(f'{string[i:j]} {string.count(string[i:j])}')
+        
             if len(string[i:j]) <= 0 or string[i:j] in L:
                 break
+
+            print(f'{string[i:j]} {count_substring(string,string[i:j])}')
+
             if string[i:j][0] in 'AEIOUaeiou':
-                kevin += string.count(string[i:j])
+                kevin += count_substring(string,string[i:j])
             else:
-                stuart += string.count(string[i:j])
+                stuart += count_substring(string,string[i:j])
 
             L.append(string[i:j])
         
     if kevin > stuart:
         print(f'Kevin {kevin}')
     elif kevin == stuart:
-        print(f'TIE in {stuart}')
+        print(f'Draw in {stuart}')
     else:
-        print(f'Stuart {stuart}')
+        print(f'Stuart {stuart} {kevin}')
+
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count += 1
+    return count
 
 if __name__ == '__main__':
     s = input()
